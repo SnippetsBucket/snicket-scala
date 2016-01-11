b4de14e72945c81babe7952c7bccfc1d9baf296d
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-01-11 18:52:03 +0900

    add ManageSnippet.jsx

--- public/js/app/routes.js
+++ public/js/app/routes.js
@@ -1,12 +1,12 @@
 import { Router, Route, browserHistory } from 'react-router';
 
 import Layout from './containers/layout/';
-import Hoge from './components/Hoge';
+import ManageSnippet from './containers/ManageSnippet/';
 
 export default (
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
-      <Route path="/hoge/" component={Hoge} />
+      <Route path="/snippet/create/" component={ManageSnippet} />
       <Route path="/*" />
     </Route>
   </Router>
