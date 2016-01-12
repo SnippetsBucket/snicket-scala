ab40449975515fc14cb9e4edc7c102b0085fc531
suzumi <ganimata39@gmail.com>
2016-01-12 10:15:48 +0900

    Add snippet create api

--- app/controllers/api/SnippetController.scala
+++ app/controllers/api/SnippetController.scala
@@ -4,14 +4,18 @@ import javax.inject.Inject
 import play.api._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
-import play.api.libs.json.{Json, Writes, Reads}
+import play.api.libs.json.{JsError, Json, Writes, Reads}
 import play.api.db.slick._
 import slick.driver.H2Driver.api._
 import slick.driver.JdbcProfile
+import models.Tables._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object SnippetController {
+
+  case class SnippetForm(title: String, text: String)
+  implicit val snippetFormRead = Json.reads[SnippetForm]
 
   case class Snippet(title: String, text: String)
   implicit val snippetWrites = Json.writes[Snippet]
@@ -26,13 +30,26 @@ class SnippetController @Inject()(val dbConfigProvider: DatabaseConfigProvider, 
 
   import SnippetController._
 
-  def list() = Action.async { implicit rs =>
-    import models.Tables._
+  def list = Action.async { implicit rs =>
     db.run(Snippets.result).map { snippets =>
       val snippetList = snippets.map { snippet =>
         Snippet(snippet.snippetTitle, snippet.snippetText)
       }
       Ok(Json.toJson(ResultList(0, snippetList)))
+    }
+  }
+
+  def create = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[SnippetForm].map { form =>
+      val nowTime = new java.sql.Timestamp(System.currentTimeMillis())
+      val snippet = SnippetsRow(0, form.title, form.text, 1, nowTime, nowTime)
+      db.run(Snippets += snippet).map { _ =>
+        Ok(Json.obj("code" -> 0, "result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      Future {
+        BadRequest(Json.obj("code" -> 1, "result" ->"failure", "error" -> JsError.toJson(e)))
+      }
     }
   }
 
