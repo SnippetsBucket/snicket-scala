c1066bd86317af6441dacbc644a0f32460fad07c
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-01-13 21:27:34 +0900

    add material-ui header

--- public/assets/js/app/components/layout/header/index.jsx
+++ public/assets/js/app/components/layout/header/index.jsx
@@ -1,28 +1,45 @@
-import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import Divider from 'material-ui/lib/divider';
+import Colors from 'material-ui/lib/styles/colors';
 
-import styles from './style.css';
+const style = {
+  backgroundColor: Colors.amber800
+};
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Header = () => (
+  <AppBar
+    style={style}
+    title="Title"
+    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    iconElementRight={
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Sign out" />
+      </IconMenu>
+    }
+  />
+);
 
-  render() {
-    return (
-      <header className={styles.wrapper}>
-        <div className={styles.main}>
-          <p>Snicket</p>
-          <input type="text" placeholder="input keywords..." />
-        </div>
-        <div className={styles.info}>
-          <Link to="/snippet/create/">create snippet</Link>
-        </div>
-      </header>
-    );
-  }
-}
-
-Header.propTypes = {};
+//<header className={styles.wrapper}>
+//  <div className={styles.main}>
+//    <p>Snicket</p>
+//    <input type="text" placeholder="input keywords..." />
+//  </div>
+//  <div className={styles.info}>
+//    <Link to="/snippet/create/">create snippet</Link>
+//  </div>
+//</header>
 
 export default Header;
--- public/assets/js/app/containers/Layout/index.jsx
+++ public/assets/js/app/containers/Layout/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
 
 import Header from '../../components/layout/header/';
 
