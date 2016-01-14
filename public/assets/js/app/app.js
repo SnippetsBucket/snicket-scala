b8a507222495163ba4ea3d1e00da3eccc8803d18
suzumi <ganimata39@gmail.com>
2016-01-14 11:44:12 +0900

    apply a TapEventPlugin

--- public/assets/js/app/app.js
+++ public/assets/js/app/app.js
@@ -1,7 +1,9 @@
 import { render } from 'react-dom';
 import Root from './containers/Root';
 import configureStore from './store/configureStore';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+injectTapEventPlugin();
 const store = configureStore();
 
 render(
