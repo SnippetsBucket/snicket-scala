404c3082451db6fdc0e8059e5e7d606e23cf3c4a
suzumi <ganimata39@gmail.com>
2016-01-15 08:00:13 +0900

    change design

--- public/assets/js/app/components/layout/header/index.jsx
+++ public/assets/js/app/components/layout/header/index.jsx
@@ -1,32 +1,67 @@
 import AppBar from 'material-ui/lib/app-bar';
 import IconButton from 'material-ui/lib/icon-button';
-import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
 import IconMenu from 'material-ui/lib/menus/icon-menu';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import Menu from 'material-ui/lib/menus/menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import Divider from 'material-ui/lib/divider';
 import Colors from 'material-ui/lib/styles/colors';
+//Icons
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import RemoveRedEye from 'material-ui/lib/svg-icons/image/remove-red-eye';
+import PersonAdd from 'material-ui/lib/svg-icons/social/person-add';
+import ContentCopy from 'material-ui/lib/svg-icons/content/content-copy';
+import ContentLink from 'material-ui/lib/svg-icons/content/link';
+import Delete from 'material-ui/lib/svg-icons/action/delete';
+import Download from 'material-ui/lib/svg-icons/file/file-download';
 
 const style = {
-  backgroundColor: Colors.amber800
+  backgroundColor: Colors.blue50,
 };
+const iconStyles = {
+  marginRight: 24,
+};
+const menuStyle = {
+  menu: {
+    marginRight: 32,
+    marginBottom: 32,
+    float: 'left',
+    position: 'relative',
+    zIndex: 0,
+  },
+  rightIcon: {
+    textAlign: 'center',
+    lineHeight: '24px',
+  },
+};
+
 
 const Header = () => (
   <AppBar
     style={style}
-    title="Title"
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    title="Snicket"
+    titleStyle={{color: Colors.black}}
     iconElementRight={
       <IconMenu
         iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
+          <IconButton
+            color={Colors.black}>
+            <MoreVertIcon />
+          </IconButton>
         }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        style={iconStyles}
+        color={Colors.black}
+        targetOrigin={{horizontal: 'left', vertical: 'middle'}}
+        anchorOrigin={{horizontal: 'left', vertical: 'middle'}}
       >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <MenuItem primaryText="Sign out" />
+            <MenuItem primaryText="Preview" leftIcon={<RemoveRedEye />} />
+            <MenuItem primaryText="Share" leftIcon={<PersonAdd />} />
+            <MenuItem primaryText="Get links" leftIcon={<ContentLink />} />
+            <Divider />
+            <MenuItem primaryText="Make a copy" leftIcon={<ContentCopy />} />
+            <MenuItem primaryText="Download" leftIcon={<Download />} />
+            <Divider />
+            <MenuItem primaryText="Remove" leftIcon={<Delete />} />
+
       </IconMenu>
     }
   />
