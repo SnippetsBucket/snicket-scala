d0367be676c12756dc48ebf2459983c4fac2a154
suzumi <ganimata39@gmail.com>
2016-01-16 08:55:10 +0900

    Add react-bootstrap

--- public/assets/js/app/components/layout/header/index.jsx
+++ public/assets/js/app/components/layout/header/index.jsx
@@ -1,21 +1,28 @@
-import AppBar from 'material-ui/lib/app-bar';
-import IconButton from 'material-ui/lib/icon-button';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
-import Menu from 'material-ui/lib/menus/menu';
-import MenuItem from 'material-ui/lib/menus/menu-item';
-import Divider from 'material-ui/lib/divider';
-import Colors from 'material-ui/lib/styles/colors';
-//Icons
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
-import RemoveRedEye from 'material-ui/lib/svg-icons/image/remove-red-eye';
-import PersonAdd from 'material-ui/lib/svg-icons/social/person-add';
-import ContentCopy from 'material-ui/lib/svg-icons/content/content-copy';
-import ContentLink from 'material-ui/lib/svg-icons/content/link';
-import Delete from 'material-ui/lib/svg-icons/action/delete';
-import Download from 'material-ui/lib/svg-icons/file/file-download';
+//import AppBar from 'material-ui/lib/app-bar';
+//import IconButton from 'material-ui/lib/icon-button';
+//import IconMenu from 'material-ui/lib/menus/icon-menu';
+//import Menu from 'material-ui/lib/menus/menu';
+//import MenuItem from 'material-ui/lib/menus/menu-item';
+//import Divider from 'material-ui/lib/divider';
+//import Colors from 'material-ui/lib/styles/colors';
+////Icons
+//import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+//import RemoveRedEye from 'material-ui/lib/svg-icons/image/remove-red-eye';
+//import PersonAdd from 'material-ui/lib/svg-icons/social/person-add';
+//import ContentCopy from 'material-ui/lib/svg-icons/content/content-copy';
+//import ContentLink from 'material-ui/lib/svg-icons/content/link';
+//import Delete from 'material-ui/lib/svg-icons/action/delete';
+//import Download from 'material-ui/lib/svg-icons/file/file-download';
+//bootstrap
+import Navbar from 'react-bootstrap/lib/Navbar';
+import NavbarHeader from 'react-bootstrap/lib/NavbarHeader';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 
 const style = {
-  backgroundColor: Colors.blue50,
+  backgroundColor: '#22354c'
 };
 const iconStyles = {
   marginRight: 24,
@@ -37,35 +44,24 @@ const menuStyle = {
 // header: #22354c
 // footer: #e6edf4
 const Header = () => (
-  <AppBar
-    style={style}
-    title="Snicket"
-    titleStyle={{color: Colors.black}}
-    iconElementRight={
-      <IconMenu
-        iconButtonElement={
-          <IconButton
-            color={Colors.black}>
-            <MoreVertIcon />
-          </IconButton>
-        }
-        style={iconStyles}
-        color={Colors.black}
-        targetOrigin={{horizontal: 'left', vertical: 'middle'}}
-        anchorOrigin={{horizontal: 'left', vertical: 'middle'}}
-      >
-            <MenuItem primaryText="Preview" leftIcon={<RemoveRedEye />} />
-            <MenuItem primaryText="Share" leftIcon={<PersonAdd />} />
-            <MenuItem primaryText="Get links" leftIcon={<ContentLink />} />
-            <Divider />
-            <MenuItem primaryText="Make a copy" leftIcon={<ContentCopy />} />
-            <MenuItem primaryText="Download" leftIcon={<Download />} />
-            <Divider />
-            <MenuItem primaryText="Remove" leftIcon={<Delete />} />
-
-      </IconMenu>
-    }
-  />
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">Snicket</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem href="#">Link</NavItem>
+        <NavItem href="#">Link</NavItem>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem >Action</MenuItem>
+          <MenuItem >Another action</MenuItem>
+          <MenuItem >Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem >Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
 );
 
 //<header className={styles.wrapper}>
