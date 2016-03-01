55b07f862a132fd30206464d6166fed9b4fed485
suzumi <ganimata39@gmail.com>
2016-03-02 01:15:09 +0900

    fix form post

--- public/assets/js/app/containers/ManageSnippet/index.jsx
+++ public/assets/js/app/containers/ManageSnippet/index.jsx
@@ -35,7 +35,7 @@ class ManageSnippet extends Component {
 
     this.props.postData('/api/snippet/create', {
       title: e.target[0].value,
-      text: e.target[1].value
+      text: e.target[2].value
     });
   }
 
@@ -44,41 +44,45 @@ class ManageSnippet extends Component {
       <div>
         <Header />
         <div className="snippet-form-wrapper">
+          <form onSubmit={this.handleSubmit}>
           <div className="container-fluid snippet-form-container">
-            <div className="row title">
-              <div className="col-sm-12">
-                <div className="snippet-form-title">
-                  <input type="text" name="title" className="form-control" placeholder="Title" required />
-                </div>
-              </div>
-            </div>
-            <div className="row content">
-              <div className="col-sm-12" style={this.styles}>
-                <div className="snippet-body-wrapper clearfix">
-                  <div className="col-sm-6 snippet-body-left">
-                    <div className="snippet-form-tabs">
-                      <span className="snippet-form-tab" style={{display: 'inline-block', backgroundColor: '#fff'}}>Write</span>
-                      <button type="button" className="comment-form-tab" data-toggle="modal" data-target="#markdown-help"><i className="fa fa-question-circle"></i>How to markdown</button>
-                    </div>
-                    <div className="snippet-form-body-panel">
-                      <textarea className="form-control snippet-form-body" onChange={this.parseMD} id="markdown-here" name="body" placeholder="Input Markdown..." required></textarea>
-                    </div>
-                  </div>
-                  <div className="col-sm-6 snippet-body-right">
-                    <div className="snippet-form-tabs">
-                      <span className="snippet-form-tab" style={{display: 'block'}}>Preview</span>
-                    </div>
-                      <SnippetDetailPreview preview={this.props.snippets.previewHtml} />
+
+              <div className="row title">
+                <div className="col-sm-12">
+                  <div className="snippet-form-title">
+                    <input type="text" name="title" className="form-control" placeholder="Title" required />
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="row submit-btn-area">
-              <div className="col-sm-12" style={this.styles}>
-                <button className="btn btn-success pull-right" style={{height: '90%'}}><i className="fa fa-pencil-square-o"></i>Post</button>
+              <div className="row content">
+                <div className="col-sm-12" style={this.styles}>
+                  <div className="snippet-body-wrapper clearfix">
+                    <div className="col-sm-6 snippet-body-left">
+                      <div className="snippet-form-tabs">
+                        <span className="snippet-form-tab" style={{display: 'inline-block', backgroundColor: '#fff'}}>Write</span>
+                        <button type="button" className="comment-form-tab" data-toggle="modal" data-target="#markdown-help"><i className="fa fa-question-circle"></i>How to markdown</button>
+                      </div>
+                      <div className="snippet-form-body-panel">
+                        <textarea className="form-control snippet-form-body" onChange={this.parseMD} id="markdown-here" name="text" placeholder="Input Markdown..." required></textarea>
+                      </div>
+                    </div>
+                    <div className="col-sm-6 snippet-body-right">
+                      <div className="snippet-form-tabs">
+                        <span className="snippet-form-tab" style={{display: 'block'}}>Preview</span>
+                      </div>
+                        <SnippetDetailPreview preview={this.props.snippets.previewHtml} />
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+              <div className="row submit-btn-area">
+                <div className="col-sm-12" style={this.styles}>
+                  <button className="btn btn-success pull-right" style={{height: '90%'}}><i className="fa fa-pencil-square-o"></i>Post</button>
+                </div>
+              </div>
+
           </div>
+          </form>
         </div>
       </div>
     );
